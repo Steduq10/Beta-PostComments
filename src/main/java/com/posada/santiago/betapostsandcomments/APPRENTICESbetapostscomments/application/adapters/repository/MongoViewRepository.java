83c353a8bcd46ebf848b6ed2fe8b11ec50c5db55
Steven Duque <steduq10@DESKTOP-RCPLA2L.localdomain>
2022-09-06 12:23:20 -0500

    updated

--- src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/application/adapters/repository/MongoViewRepository.java
+++ src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/application/adapters/repository/MongoViewRepository.java
@@ -2,13 +2,19 @@ package com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.ap
 
 
 import com.google.gson.Gson;
+import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.application.handlers.QueryHandler;
 import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.business.gateways.DomainViewRepository;
 import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.business.gateways.model.CommentViewModel;
 import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.business.gateways.model.PostViewModel;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Repository
 public class MongoViewRepository implements DomainViewRepository {
@@ -35,7 +41,7 @@ public class MongoViewRepository implements DomainViewRepository {
     @Override
     public Mono<PostViewModel> saveNewPost(PostViewModel post) {
         /** make the implementation, using the template, to save a post*/
-        return null;
+        return template.save(post);
     }
 
     @Override
@@ -43,6 +49,14 @@ public class MongoViewRepository implements DomainViewRepository {
         /** make the implementation, using the template, to find the post in the database that you want to add the comment to,
          * then add the comment to the list of comments and using the Update class update the existing post
          * with the new list of comments*/
-        return null;
+        var query = new Query(Criteria.where("aggregateId").is(comment.getPostId()));
+        Update update = new Update();
+        return template.findOne(query, PostViewModel.class)
+                .flatMap(postViewModel -> {
+                    List<CommentViewModel> comments = postViewModel.getComments();
+                    comments.add(comment);
+                    update.set("comments", comments);
+                    return template.findAndModify(query, update, PostViewModel.class);
+                });
     }
 }
--- src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/application/handlers/QueueHandler.java
+++ src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/application/handlers/QueueHandler.java
@@ -2,7 +2,9 @@ package com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.ap
 
 
 
+import co.com.sofka.domain.generic.DomainEvent;
 import com.google.gson.Gson;
+import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.application.adapters.bus.Notification;
 import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.business.usecases.UpdateViewUseCase;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,13 @@ public class QueueHandler implements Consumer<String> {
     @Override
     public void accept(String received) {
         //Finish the implementation of this Method
+        Notification notification = gson.fromJson(received, Notification.class);
+        String type = notification.getType().replace("alphapostsandcomments", "betapostsandcomments.APPRENTICESbetapostscomments");
+        try {
+            DomainEvent event = (DomainEvent) gson.fromJson(notification.getBody(), Class.forName(type));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
